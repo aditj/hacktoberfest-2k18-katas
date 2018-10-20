0f278ed4e95591deafca62fe75e087a2bfa9c952
Adit Jain <aditjain1980@gmail.com>
2018-10-20 20:56:26 +0530

    completed The camelCase function

--- src/camelCase.js
+++ src/camelCase.js
@@ -1,3 +1,11 @@
 export const camelCase = () => {
   // TO IMPLEMENT IN ANOTHER PR
+    var strArr = str.split('');
+  
+    var stri='';
+    for(var i in strArr ){
+      var n = i.length;
+      stri+=i[0].toUpperCase()+ i.slice(1, n);
+    }
+  return stri;
 };
